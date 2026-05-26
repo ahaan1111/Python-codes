c990c028e20312469442ac5a20a9bff80a659801
ahaan1111 <ahaanpamnani@gmail.com>
2026-05-26 10:23:39 -0500

    Add files via upload

---
+++ not.py
@@ -0,0 +1,17 @@
+a= 3
+b= 6
+c= 6
+if not(a==b):
+    print (" a does not equal b")
+else:
+    print (" a equals b")
+if not (b==c):
+    print (" b does not equal c")
+else:
+    print ("b equals c")
+a= "python"
+b= "coding"
+if not (a==b):
+    print ("a does not equal b")
+else: 
+    print (" a equals b ")
