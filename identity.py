8e4fdf64aa33e672fec55bbe0490887951c71624
ahaan1111 <ahaanpamnani@gmail.com>
2026-05-29 10:06:36 -0500

    Add files via upload

---
+++ identity.py
@@ -0,0 +1,15 @@
+a = [1, 2, 3, 4, 5]
+b = [1, 2, 3, 4, 5]
+c=a 
+print (a is c)
+print (a is b)
+print (b is c)
+print (a is not c)
+print (a is not b)
+print (b is not c)
+print (a in c)
+print (a in b)
+print (b in c)
+print (a not in c)
+print (a not in b)
+print (b not in c)
