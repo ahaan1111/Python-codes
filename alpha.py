6bbd5276923b2c1d07157d8697fa1ede58898315
ahaan1111 <ahaanpamnani@gmail.com>
2026-05-29 09:40:04 -0500

    Add files via upload

---
+++ alpha.py
@@ -0,0 +1,12 @@
+charact = input("input a character ")
+print (charact)
+
+if (charact.isalpha()):
+    print ("user string is alphabet")
+elif (charact.isdecimal()):
+    print ("user string is number")
+else :
+    print ("user string is not alphabet nor number")
+
+
+    
