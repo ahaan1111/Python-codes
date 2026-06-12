163ec8552f48da65c2f9d0c5839bc1b6ed479eba
ahaan1111 <ahaanpamnani@gmail.com>
2026-06-12 10:04:13 -0500

    Add files via upload

---
+++ expression.py
@@ -0,0 +1,16 @@
+v=3
+f=4
+g=6
+a=1
+b=9
+m= (v + f) * g / a*(b)
+print (" the result of the expression is ", m)
+
+
+name= "eathan"
+age=1
+
+if name== "jake" or "alex" or age >= 2:
+    print ("welcome ")
+else: 
+    print ("goodbye")
