1638e379e9cab240e5c3240628136226f85ad182
ahaan1111 <ahaanpamnani@gmail.com>
2026-06-12 10:17:34 -0500

    Add files via upload

---
+++ divistest.py
@@ -0,0 +1,7 @@
+dividend= int (input("enter the dividend "))
+divisor= int (input("enter the divisor "))
+
+if (dividend%divisor == 0): 
+    print (dividend," is divisible by ", divisor)
+else: 
+    print ("it is not divisible")
