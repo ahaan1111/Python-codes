65a1f5ca4407cba7af3d682dc9c37e3fccc98ed6
ahaan1111 <ahaanpamnani@gmail.com>
2026-06-12 10:26:53 -0500

    Add files via upload

---
+++ mean.py
@@ -0,0 +1,8 @@
+wrongmean = 38
+wrongnumber = 36
+correctnumber = 56
+total= 40
+wrongsum = total*wrongmean
+correctsum= (wrongsum-wrongnumber) + correctnumber
+correctmean= correctsum*total
+print ("correct mean equals ", correctmean)
