8bfb0f9c0eb0793858ea9a0e79293465d7d8a48f
ahaan1111 <ahaanpamnani@gmail.com>
2026-06-30 21:16:01 -0500

    Add files via upload

---
+++ game.py
@@ -0,0 +1,11 @@
+secret = 25
+input ("enter your guess")
+if input <=5 from secret or >= 5 from secret:
+    then:
+    print: ("hot")
+if input <=10 from secret or >= 10 from secret:
+then:
+print ("warm")
+if if input <=15 from secret or >= 15 from secret:
+then: 
+
