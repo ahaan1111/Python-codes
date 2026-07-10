67c1bf57042aa0424047d0c5c345ea57cb664c32
ahaan1111 <ahaanpamnani@gmail.com>
2026-07-10 10:42:25 -0500

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,6 @@
+def Weather_Condition():
+    print ("the weather is pleasant in spring", spring)
+    print ("the weather is same in autumn", autumn)
+spring= "autumn"
+autumn= "spring"
+Weather_Condition()
